bf8a58e6fd3714bbfd825ac792ecd5d927a3e88a
anaceciliaa <anacecillia303@gmail.com>
2023-06-26 15:49:40 -0300

    introduce parameter object - 1, extrac class - 1

--- src/control/Funcionario.java
+++ src/control/Funcionario.java
@@ -9,58 +9,152 @@ package control;
  *
  * @author FERNANDO ROGI
  */
-public class Funcionario {
 
+class DadosFuncionario {
+    private String nome;
+    private String cpf;
+    private String email;
+    private String fone;
+    private String idade;
+    private String salario;
+    private String fonec;
+    private String numero;
+    private String data;
+    private String departamento;
+    private String funcao;
+    private String observacao;
+    private String sexo;
+
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return this.cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFone() {
+        return this.fone;
+    }
+
+    public void setFone(String fone) {
+        this.fone = fone;
+    }
+
+    public String getIdade() {
+        return this.idade;
+    }
+
+    public void setIdade(String idade) {
+        this.idade = idade;
+    }
+
+    public String getSalario() {
+        return this.salario;
+    }
+
+    public void setSalario(String salario) {
+        this.salario = salario;
+    }
+
+    public String getFonec() {
+        return this.fonec;
+    }
+
+    public void setFonec(String fonec) {
+        this.fonec = fonec;
+    }
+
+    public String getNumero() {
+        return this.numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getData() {
+        return this.data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public String getDepartamento() {
+        return this.departamento;
+    }
+
+    public void setDepartamento(String departamento) {
+        this.departamento = departamento;
+    }
+
+    public String getFuncao() {
+        return this.funcao;
+    }
+
+    public void setFuncao(String funcao) {
+        this.funcao = funcao;
+    }
+
+    public String getObservacao() {
+        return this.observacao;
+    }
+
+    public void setObservacao(String observacao) {
+        this.observacao = observacao;
+    }
+
+    public String getSexo() {
+        return this.sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public DadosFuncionario(String nome, String cpf, String email, String fone,  String idade, String salario, String fonec, String numero, String data, String departamento, String funcao, String observacao, String sexo) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.email = email;
+        this.fone = fone;
+        this.idade = idade;
+        this.salario = salario;
+        this.fonec = fonec;
+        this.numero = numero;
+        this.data = data;
+        this.departamento = departamento;
+        this.funcao = funcao;
+        this.observacao = observacao;
+        this.sexo = sexo;
+    }   
+    
+}
+
+public class Funcionario {
     public static String v[] = new String[14];
-    public String nome;
-    public String cpf;
-    public String email;
-    public String fone;
-    public String idade;
-    public String salario;
-    public String fonec;
-    public String numero;
-    public String data;
-    public String departamento;
-    public String funcao;
-    public String observacao;
-    public String sexo;
      public static String id = "0";
+     private DadosFuncionario dadosFuncionario;
     
-    public Funcionario(){}
-    
-    public Funcionario(
-    
-    String nome,
-    String cpf,
-    String email,
-    String fone,
-    String idade,
-    String salario,
-    String fonec,
-    String numero,
-    String data,
-    String departamento,
-    String funcao,
-    String obcervacao,
-    String sexo,
-    String id
-            
-    ){
-    this.nome = nome;
-    this.cpf = cpf;
-    this.email = email;
-    this.fone = fone;
-    this.idade = idade;
-    this.salario = salario;
-    this.fonec = fonec;
-    this.numero = numero;
-    this.data = data;
-    this.departamento = departamento;
-    this.funcao = funcao;
-    this.observacao = obcervacao;
-    this.sexo = sexo;
-    this.id = id;
+    public Funcionario(DadosFuncionario dadosFuncionario) {
+        this.dadosFuncionario = dadosFuncionario;
     }
 
     public String getId() {
@@ -70,109 +164,14 @@ public class Funcionario {
     public void setId(String id) {
         this.id = id;
     }
-    
-    public String getNome() {
-        return nome;
+
+    public DadosFuncionario getDadosFuncionario() {
+        return dadosFuncionario;
     }
 
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getCpf() {
-        return cpf;
-    }
-
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getFone() {
-        return fone;
+    public void setDadosFuncionario(DadosFuncionario dadosFuncionario) {
+        this.dadosFuncionario = dadosFuncionario;
     }
     
-    public void setFone(String fone) {
-        this.fone = fone;
-    }
-
-    public String getIdade() {
-        return idade;
-    }
-
-    public void setIdade(String idade) {
-        this.idade = idade;
-    }
-
-    public String getSalario() {
-        return salario;
-    }
-
-    public void setSalario(String salario) {
-        this.salario = salario;
-    }
-
-    public String getFonec() {
-        return fonec;
-    }
-    
-    public void setFonec(String fonec){
-        this.fonec = fonec;
-    }
-    
-    public String getNumero(){
-        return numero;
-    }
-    
-    public void setNumero(String numero) {
-        this.numero = numero;
-    }
-
-    public String getData() {
-        return data;
-    }
-
-    public void setData(String data) {
-        this.data = data;
-    }
-
-    public String getDepartamento() {
-        return departamento;
-    }
-
-    public void setDepartamento(String departamento) {
-        this.departamento = departamento;
-    }
-
-    public String getFuncao() {
-        return funcao;
-    }
-
-    public void setFuncao(String funcao) {
-        this.funcao = funcao;
-    }
-
-    public String getObservacao() {
-        return observacao;
-    }
-
-    public void setObservacao(String observacao) {
-        this.observacao = observacao;
-    }
-
-    public String getSexo() {
-        return sexo;
-    }
-
-    public void setSexo(String sexo) {
-        this.sexo = sexo;
-    }
 
 }
