0c41f3afed2d6aa7644b1866e3538bfe9e7429da
anaceciliaa <anacecillia303@gmail.com>
2023-06-27 11:17:54 -0300

    extract method - 1

--- src/model/ClienteWrite.java
+++ src/model/ClienteWrite.java
@@ -8,45 +8,37 @@ import static view.ClienteView.Modelo;
 import java.util.ArrayList;
 
 public class ClienteWrite {
-    
-         public static ArrayList<Cliente> cadastros = new ArrayList<>();
-         public static Cliente cadastro = new Cliente();
+   public static ArrayList<Cliente> cadastros = new ArrayList<>();
+   public static Cliente cadastro = new Cliente(null);
          
-            public static void salvar(){
+   public static void salvar(){
+      atualizarDadosCliente();
+      atualizarModelo();
+   }
             
-            ClienteWrite.cadastros.get(ClienteView.index).nome = Cliente.v[0];
-            ClienteWrite.cadastros.get(ClienteView.index).cpf = Cliente.v[1];
-            ClienteWrite.cadastros.get(ClienteView.index).fone = Cliente.v[2];
-            ClienteWrite.cadastros.get(ClienteView.index).idade = Cliente.v[3];
-            ClienteWrite.cadastros.get(ClienteView.index).cidade = Cliente.v[4];
-            ClienteWrite.cadastros.get(ClienteView.index).sexo = Cliente.v[5];
-            ClienteWrite.cadastros.get(ClienteView.index).fonec = Cliente.v[6];
-            ClienteWrite.cadastros.get(ClienteView.index).email = Cliente.v[7];
-            ClienteWrite.cadastros.get(ClienteView.index).cep = Cliente.v[8];
-            ClienteWrite.cadastros.get(ClienteView.index).estado = Cliente.v[9];
-            ClienteWrite.cadastros.get(ClienteView.index).bairro = Cliente.v[10];
-            ClienteWrite.cadastros.get(ClienteView.index).numero = Cliente.v[11];
-            ClienteWrite.cadastros.get(ClienteView.index).rua = Cliente.v[12];
-            
-            Cliente.id = ClienteWrite.cadastros.get(ClienteView.index).cpf;
-        
-            cadastro.setNome(Cliente.v[0]);
-            cadastro.setCpf(Cliente.v[1]);
-            cadastro.setFone(Cliente.v[2]);
-            cadastro.setCidade(Cliente.v[3]);
-            cadastro.setIdade(Cliente.v[4]);
-            cadastro.setSexo(Cliente.v[5]);
-            cadastro.setFonec(Cliente.v[6]);
-            cadastro.setEmail(Cliente.v[7]);
-            cadastro.setCep(Cliente.v[8]);
-            cadastro.setEstado(Cliente.v[9]);
-            cadastro.setBairro(Cliente.v[10]);
-            cadastro.setNumero(Cliente.v[11]);
-            cadastro.setRua(Cliente.v[12]);
-            
-            ClienteWrite.cadastros.set(ClienteView.index, cadastro);
-            Modelo.setValueAt(cadastro, ClienteView.index);
-                
-            } 
-         
+
+   private static void atualizarDadosCliente() {
+      Cliente cliente = ClienteWrite.cadastros.get(ClienteView.index);
+      String[] dadosCliente = Cliente.v;
+      
+      cliente.getDadosCliente().setNome(dadosCliente[0]);
+      cliente.getDadosCliente().setCpf(dadosCliente[1]);
+      cliente.getDadosCliente().setFone(dadosCliente[2]);
+      cliente.getDadosCliente().setIdade(dadosCliente[3]);
+      cliente.getDadosCliente().setCidade(dadosCliente[4]);
+      cliente.getDadosCliente().setSexo(dadosCliente[5]);
+      cliente.getDadosCliente().setFonec(dadosCliente[6]);
+      cliente.getDadosCliente().setEmail(dadosCliente[7]);
+      cliente.getDadosCliente().setCep(dadosCliente[8]);
+      cliente.getDadosCliente().setEstado(dadosCliente[9]);
+      cliente.getDadosCliente().setBairro(dadosCliente[10]);
+      cliente.getDadosCliente().setNumero(dadosCliente[11]);
+      cliente.getDadosCliente().setRua(dadosCliente[12]);
+      Cliente.id = cliente.getDadosCliente().getCpf();
+}
+
+   private static void atualizarModelo() {
+      Cliente cliente = ClienteWrite.cadastros.get(ClienteView.index);
+      ClienteView.Modelo.setValueAt(cliente, ClienteView.index);
+}
 }
