dbfcdf0dcba487962f0e13a01bd340885a27b1c8
anaceciliaa <anacecillia303@gmail.com>
2023-06-27 14:24:22 -0300

    extract method - 1

--- src/tablemodel/ClienteWriteTM.java
+++ src/tablemodel/ClienteWriteTM.java
@@ -12,36 +12,43 @@ import javax.swing.JOptionPane;
 public class ClienteWriteTM {
     
         public static void writeTM(){
-        
-        Cliente cadastroTM = new Cliente();
-        cadastroTM.setNome(Cliente.v[0]);
-        cadastroTM.setCpf(Cliente.v[1]);
-        cadastroTM.setFone(Cliente.v[2]);
-        cadastroTM.setIdade(Cliente.v[3]);
-        cadastroTM.setCidade(Cliente.v[4]);
+            Cliente cliente = criarCliente();
 
-        if (null != Cliente.v[5]) switch (Cliente.v[5]) {
-                case "Masculino":
-                    cadastroTM.setSexo("Masculino");
-                    break;
-                case "Feminino":
-                    cadastroTM.setSexo("Feminino");
-                    break;
-                default:
-                    JOptionPane.showMessageDialog(null, "Sexo não informado");
-                    cadastroTM.setSexo("NÃO INFORMADO");
-                    break;
+            if(cliente.getDadosCliente().getSexo() == null) {
+                JOptionPane.showMessageDialog(null, "Sexo não informado");
+                cliente.getDadosCliente().setSexo("NÃO INFORMADO");
             }
 
-        cadastroTM.setFonec(Cliente.v[6]);
-        cadastroTM.setEmail(Cliente.v[7]);
-        cadastroTM.setCep(Cliente.v[8]);
-        cadastroTM.setEstado(Cliente.v[9]);
-        cadastroTM.setBairro(Cliente.v[10]);
-        cadastroTM.setNumero(Cliente.v[11]);
-        cadastroTM.setRua(Cliente.v[12]);
-
-        ClienteView.Modelo.addFuncionario(cadastroTM);
-            
+            ClienteView.Modelo.addFuncionario(cliente);
         }
+
+        private static Cliente criarCliente() {
+            Cliente cliente = new Cliente(null);
+            
+            cliente.getDadosCliente().setNome(Cliente.v[0]);
+            cliente.getDadosCliente().setCpf(Cliente.v[1]);
+            cliente.getDadosCliente().setFone(Cliente.v[2]);
+            cliente.getDadosCliente().setIdade(Cliente.v[3]);
+            cliente.getDadosCliente().setCidade(Cliente.v[4]);
+            cliente.getDadosCliente().setSexo(obterSexo(Cliente.v[5]));
+            cliente.getDadosCliente().setFonec(Cliente.v[6]);
+            cliente.getDadosCliente().setEmail(Cliente.v[7]);
+            cliente.getDadosCliente().setCep(Cliente.v[8]);
+            cliente.getDadosCliente().setEstado(Cliente.v[9]);
+            cliente.getDadosCliente().setBairro(Cliente.v[10]);
+            cliente.getDadosCliente().setNumero(Cliente.v[11]);
+            cliente.getDadosCliente().setRua(Cliente.v[12]);
+        
+            return cliente;
+        }
+
+    private static String obterSexo(String valor) {
+        if("Masculino".equals(valor)) {
+            return "Masculino";
+        }else if("Feminino".equals(valor)) {
+            return "Feminino";
+        }else {
+            return null;
+        }
+    }
 }
